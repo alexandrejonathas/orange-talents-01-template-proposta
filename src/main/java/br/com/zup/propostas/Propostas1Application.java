3f8c2bbd0648e9c95c5d64ee2887907b51dc66a4
Jonathas Lima <alexandrejonathas@gmail.com>
2021-02-09 14:41:50 -0300

    Primeiro commit

---
+++ src/main/java/br/com/zup/propostas/Propostas1Application.java
@@ -0,0 +1,13 @@
+package br.com.zup.propostas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Propostas1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Propostas1Application.class, args);
+	}
+
+}
