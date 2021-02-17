7c0a9408d1e4e502e9b7026bc060321a4616ff39
Jonathas Lima <alexandrejonathas@gmail.com>
2021-02-17 18:09:48 -0300

    Comitando integração da api com keycloak

---
+++ src/main/java/br/com/zup/propostas/security/SecurityConfig.java
@@ -0,0 +1,25 @@
+package br.com.zup.propostas.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/actuator/health").permitAll()
+                .antMatchers(HttpMethod.GET, "/propostas/**").hasAuthority("SCOPE_read")
+                .antMatchers(HttpMethod.POST, "/propostas/**").hasAuthority("SCOPE_write")
+                .anyRequest().authenticated()
+                .and()
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    }
+
+}
